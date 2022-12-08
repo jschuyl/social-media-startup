d9f8a0026830478d0d95a917559d55101c0e7d01
jschuyl <schuylertjed@gmail.com>
2022-12-08 12:26:25 -0700

    added just a bit more to student model

--- models/User.js
+++ models/User.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const User = new Schema ({
-    name: String,
-    
+    username: {
+        type: String,
+        required: true,
+        max_length: 25
+    },
+    email: {
+        type: String,
+        required: true,
+    }
+
 })
